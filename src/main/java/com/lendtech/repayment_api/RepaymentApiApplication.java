5930ffc05dd585d2f97afbe2c0c150c81e64199a
Annaliskirwa <annaliskirwa@gmail.com>
2023-02-14 23:20:59 +0200

    Repayment project initialization

---
+++ src/main/java/com/lendtech/repayment_api/RepaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.lendtech.repayment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepaymentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepaymentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lendtech/repayment_api/RepaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lendtech.repayment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepaymentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
